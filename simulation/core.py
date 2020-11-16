e9b8a31d05c1ebc0d483c3585fe5ce9918139c24
Jonas Spinner <jonas.spinner@student.kit.edu>
2020-11-16 23:39:20 +0100

    Added Mesa simulation example.

--- simulation/core.py
+++ simulation/core.py
@@ -1,10 +1,12 @@
+from mesa_example import run_mesa_example, plot_mesa_example
 
 
 def print_hi(name: str) -> None:
     """
     This function says hi to `name`.
 
-    :param name:
-    :return:
+    Parameters
+    ----------
+        name: The name to be greeted
     """
     print(f'Hi, {name}!')
--- simulation/main.py
+++ simulation/main.py
@@ -5,10 +5,27 @@ It can be executed by running `python3 ./simulation/main.py`
 """
 
 from core import print_hi
+from core import run_mesa_example, plot_mesa_example
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 
 def main() -> None:
+    # Usage of the `print_hi` function defined in `core.py`
     print_hi("Python script")
+
+    # This is an example of importing simulation code, executing it and plotting it in a python script. The simulation
+    # takes 245 iterations and about 3 minutes.
+    run_data = run_mesa_example()
+
+    fig, ax = plt.subplots()
+    plot_mesa_example(run_data, ax)
+
+    # The plot is written to a file.
+    results_dir = Path(__file__).parent.parent / "results"
+    results_dir.mkdir(exist_ok=True)
+
+    plt.savefig(results_dir / "mesa-example-gini.pdf")
 
 
 if __name__ == '__main__':
