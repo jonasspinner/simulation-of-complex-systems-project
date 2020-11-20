58e70673635cbb17ddd9df045b333b4e81ab4c39
Felix Ericsson <felix.ericsson@hotmail.com>
2020-11-20 12:28:26 +0100

    Second version of reading map layout from textfile, now with choice of resolution.

--- simulation/mapLayout.py
+++ simulation/mapLayout.py
@@ -1,26 +1,64 @@
 import numpy as np
 row = 0
 col = 0
-
+resolution = 5 #This decides how many squares each thing from the text file will be in the matrix
 file = open("map1")
-
-matrix = np.zeros((36, 60))
+iteratorRow = 0
+iteratorCol = 0
+matrix = np.zeros((36*resolution, 60*resolution))
 for line in file:
     for character in line:
         if character == " ":
-            matrix[row, col] = 0
+            while iteratorCol < resolution:
+                while iteratorRow < resolution:
+                    matrix[row + iteratorRow, col + iteratorCol] = 0
+                    iteratorRow = iteratorRow +1
+                iteratorCol = iteratorCol +1
+                iteratorRow = 0
+
         if character == "T":
-            matrix[row, col] = 1
+            while iteratorCol < resolution:
+                while iteratorRow < resolution:
+                    matrix[row + iteratorRow, col + iteratorCol] = 1
+                    iteratorRow = iteratorRow +1
+                iteratorCol = iteratorCol +1
+                iteratorRow = 0
+
         if character == "C":
-            matrix[row, col] = 2
+            while iteratorCol < resolution:
+                while iteratorRow < resolution:
+                    matrix[row + iteratorRow, col + iteratorCol] = 2
+                    iteratorRow = iteratorRow +1
+                iteratorCol = iteratorCol +1
+                iteratorRow = 0
+
         if character == "X":
-            matrix[row, col] = 3
+            while iteratorCol < resolution:
+                while iteratorRow < resolution:
+                    matrix[row + iteratorRow, col + iteratorCol] = 3
+                    iteratorRow = iteratorRow +1
+                iteratorCol = iteratorCol +1
+                iteratorRow = 0
+
         if character == "E":
-            matrix[row, col] = 4
+            while iteratorCol < resolution:
+                while iteratorRow < resolution:
+                    matrix[row + iteratorRow, col + iteratorCol] = 4
+                    iteratorRow = iteratorRow +1
+                iteratorCol = iteratorCol +1
+                iteratorRow = 0
+
         if character == "F":
-            matrix[row, col] = 5
-        col = col + 1
-    row = row + 1
+            while iteratorCol < resolution:
+                while iteratorRow < resolution:
+                    matrix[row + iteratorRow, col + iteratorCol] = 5
+                    iteratorRow = iteratorRow +1
+                iteratorCol = iteratorCol +1
+                iteratorRow = 0
+
+        col = col + resolution
+        iteratorCol = 0
+    row = row + resolution
     col = 0
 
 print(matrix)
