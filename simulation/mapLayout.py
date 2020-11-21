a663842fe43db402be89b2c2835897357f8d9224
Mattias Berg <mattias_berg@live.se>
2020-11-21 19:27:45 +0100

    Updated mapLayour with values corresponding to environment used in path and changed it to be a function

--- simulation/mapLayout.py
+++ simulation/mapLayout.py
@@ -1,64 +1,75 @@
 import numpy as np
-row = 0
-col = 0
-resolution = 5 #This decides how many squares each thing from the text file will be in the matrix
-file = open("map1")
-iteratorRow = 0
-iteratorCol = 0
-matrix = np.zeros((36*resolution, 60*resolution))
-for line in file:
-    for character in line:
-        if character == " ":
-            while iteratorCol < resolution:
-                while iteratorRow < resolution:
-                    matrix[row + iteratorRow, col + iteratorCol] = 0
-                    iteratorRow = iteratorRow +1
-                iteratorCol = iteratorCol +1
-                iteratorRow = 0
 
-        if character == "T":
-            while iteratorCol < resolution:
-                while iteratorRow < resolution:
-                    matrix[row + iteratorRow, col + iteratorCol] = 1
-                    iteratorRow = iteratorRow +1
-                iteratorCol = iteratorCol +1
-                iteratorRow = 0
+def load_environment(fileName, resolution) -> np.ndarray:
 
-        if character == "C":
-            while iteratorCol < resolution:
-                while iteratorRow < resolution:
-                    matrix[row + iteratorRow, col + iteratorCol] = 2
-                    iteratorRow = iteratorRow +1
-                iteratorCol = iteratorCol +1
-                iteratorRow = 0
+    Tile = int
 
-        if character == "X":
-            while iteratorCol < resolution:
-                while iteratorRow < resolution:
-                    matrix[row + iteratorRow, col + iteratorCol] = 3
-                    iteratorRow = iteratorRow +1
-                iteratorCol = iteratorCol +1
-                iteratorRow = 0
+    WALL:  Tile = 0
+    FLOOR: Tile = 1
+    SEAT:  Tile = 2
+    ENTRY: Tile = 3
+    FOOD:  Tile = 4
 
-        if character == "E":
-            while iteratorCol < resolution:
-                while iteratorRow < resolution:
-                    matrix[row + iteratorRow, col + iteratorCol] = 4
-                    iteratorRow = iteratorRow +1
-                iteratorCol = iteratorCol +1
-                iteratorRow = 0
-
-        if character == "F":
-            while iteratorCol < resolution:
-                while iteratorRow < resolution:
-                    matrix[row + iteratorRow, col + iteratorCol] = 5
-                    iteratorRow = iteratorRow +1
-                iteratorCol = iteratorCol +1
-                iteratorRow = 0
-
-        col = col + resolution
-        iteratorCol = 0
-    row = row + resolution
+    row = 0
     col = 0
+    resolution = resolution #This decides how many squares each thing from the text file will be in the matrix
+    file = open(fileName)
+    iteratorRow = 0
+    iteratorCol = 0
+    matrix = np.zeros((36*resolution, 60*resolution))
+    for line in file:
+        for character in line:
+            if character == " ":
+                while iteratorCol < resolution:
+                    while iteratorRow < resolution:
+                        matrix[row + iteratorRow, col + iteratorCol] = FLOOR
+                        iteratorRow = iteratorRow +1
+                    iteratorCol = iteratorCol +1
+                    iteratorRow = 0
 
-print(matrix)
+            if character == "T":
+                while iteratorCol < resolution:
+                    while iteratorRow < resolution:
+                        matrix[row + iteratorRow, col + iteratorCol] = WALL
+                        iteratorRow = iteratorRow +1
+                    iteratorCol = iteratorCol +1
+                    iteratorRow = 0
+
+            if character == "C":
+                while iteratorCol < resolution:
+                    while iteratorRow < resolution:
+                        matrix[row + iteratorRow, col + iteratorCol] = SEAT
+                        iteratorRow = iteratorRow +1
+                    iteratorCol = iteratorCol +1
+                    iteratorRow = 0
+
+            if character == "X":
+                while iteratorCol < resolution:
+                    while iteratorRow < resolution:
+                        matrix[row + iteratorRow, col + iteratorCol] = WALL
+                        iteratorRow = iteratorRow +1
+                    iteratorCol = iteratorCol +1
+                    iteratorRow = 0
+
+            if character == "E":
+                while iteratorCol < resolution:
+                    while iteratorRow < resolution:
+                        matrix[row + iteratorRow, col + iteratorCol] = ENTRY
+                        iteratorRow = iteratorRow +1
+                    iteratorCol = iteratorCol +1
+                    iteratorRow = 0
+
+            if character == "F":
+                while iteratorCol < resolution:
+                    while iteratorRow < resolution:
+                        matrix[row + iteratorRow, col + iteratorCol] = FOOD
+                        iteratorRow = iteratorRow +1
+                    iteratorCol = iteratorCol +1
+                    iteratorRow = 0
+
+            col = col + resolution
+            iteratorCol = 0
+        row = row + resolution
+        col = 0
+
+    return matrix
