2cdfcd5a45f59926742b23874c6f87d41b6b8645
Jonas Spinner <jonas.spinner@student.kit.edu>
2020-11-21 20:19:57 +0100

    Made graph building more efficient.

--- simulation/path.py
+++ simulation/path.py
@@ -1,104 +1,65 @@
 from itertools import product
-import numpy as np
+from typing import Tuple, Optional, List, TypeVar
+
 import networkx as nx
-from pathfinding.core.diagonal_movement import DiagonalMovement
-from pathfinding.core.grid import Grid
-from pathfinding.finder.a_star import AStarFinder
+import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
-from typing import Tuple, Optional, List, TypeVar
 
 Tile = int
 Pos = Tuple[int, int]
 T = TypeVar("T")
 
-WALL:  Tile = 0
+WALL: Tile = 0
 FLOOR: Tile = 1
-SEAT:  Tile = 2
+SEAT: Tile = 2
 ENTRY: Tile = 3
-FOOD:  Tile = 4
+FOOD: Tile = 4
 
 building_cmap = ListedColormap([
     [0.5, 0.5, 0.5, 1],  # wall
     [1, 1, 1, 1],  # floor
     [1, 0, 0, 1],  # seat
     [0, 1, 0, 1],  # entry
-    [0, 0, 1, 1]   # food
+    [0, 0, 1, 1]  # food
 ])
 
-
-def load_environment() -> np.ndarray:
-    return np.array([
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 2, 0, 2, 1, 1, 1, 2, 0, 2, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 2, 0, 2, 1, 1, 1, 2, 0, 2, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 2, 0, 2, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-        [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 0],
-        [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 2, 0, 2, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 2, 0, 2, 1, 1, 1, 2, 0, 2, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 2, 0, 2, 1, 1, 1, 2, 0, 2, 1, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ])
-
-#plt.imshow(load_environment(), cmap=building_cmap)
-#plt.show()
-
-def find_path_pathlib(environment: np.ndarray, start: Pos, end: Pos) -> List[Pos]:
-    # `grid` could be reused.
-    # The values of the matrix signify the walking cost and a cost of zero signifies unpassable tiles.
-    # Giving seats, entries and food a higher cost prevents agents from walking over seats.
-    matrix = np.ones_like(environment)
-    matrix[environment == WALL]  = 0
-    matrix[environment == SEAT]  = 10
-    matrix[environment == ENTRY] = 10
-    matrix[environment == FOOD]  = 10
-    grid = Grid(matrix=matrix.T)  # The coordinates for the numpy array and pathfinding are flipped.
-
-    start = grid.node(*start)
-    end = grid.node(*end)
-
-    finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
-    path, runs = finder.find_path(start, end, grid)
-    return path
 
 def distance(u: Pos, v: Pos) -> float:
     return np.linalg.norm(np.array(u) - np.array(v))
 
+
 def build_graph(environment: np.ndarray) -> nx.DiGraph:
-
-    Tile = int
-
-    WALL:  Tile = 0
-    FLOOR: Tile = 1
-    SEAT:  Tile = 2
-    ENTRY: Tile = 3
-    FOOD:  Tile = 4
-
-    width, height = environment.shape
-    positions = list(product(range(width), range(height)))
-
+    height, width = environment.shape
+    positions = list(product(range(height), range(width)))
+    print(positions[-1], height, width)
     graph = nx.DiGraph()
     graph.add_nodes_from(positions)
 
+    def neighbors(pos: Pos) -> List[Pos]:
+        i, j = pos
+        candidates = [(i + di, j + dj) for di, dj
+                      in [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]]
+        return [(i, j) for i, j in candidates
+                if 0 <= i < height and 0 <= j < width and environment[i, j] != WALL]
+
     for u in positions:
-        for v in positions:
+        if environment[u] == WALL:
+            continue
+        for v in neighbors(u):
             d = distance(u, v)
-            if d <= 1.5 and u != v and environment[u] != WALL and environment[v] != WALL:
+            if d <= 1.5:
                 if environment[u] != FLOOR or environment[v] != FLOOR:
                     d += 10
                 graph.add_edge(u, v, distance=d)
-
     return graph
 
-def find_path_networkx(environment: nx.networkx, start: Pos, end: Pos) -> List[Pos]:
-    print("-----")
+
+def find_path(environment: nx.networkx, start: Pos, end: Pos) -> List[Pos]:
     graph = build_graph(environment)
-    print("-----")
     # noinspection PyTypeChecker
     return nx.astar_path(graph, start, end, heuristic=distance, weight="distance")
+
 
 def plot_path(environment: np.ndarray, path: Optional[List[Pos]] = None) -> None:
     plt.imshow(environment, cmap=building_cmap)
@@ -106,26 +67,3 @@ def plot_path(environment: np.ndarray, path: Optional[List[Pos]] = None) -> None
         ys, xs = zip(*path)
         plt.plot(xs, ys, "k-")
     plt.show()
-
-def choose(options: List[T]) -> T:
-    return options[np.random.randint(len(options))]
-
-def select_tiles(environment: np.ndarray, tile_type: Tile) -> List[Tile]:
-    # noinspection PyTypeChecker
-    return list(zip(*np.where(environment == tile_type)))
-
-def simulate_agent() -> None:
-    environment = load_environment()
-
-    entries = select_tiles(environment, ENTRY)
-    seats   = select_tiles(environment, SEAT)
-    foods   = select_tiles(environment, FOOD)
-
-    # A random choice of waypoints.
-    waypoints = [choose(entries), seat := choose(seats), choose(foods), seat, choose(entries)]
-
-    for i in range(len(waypoints) - 1):
-        path = find_path_networkx(environment, waypoints[i], waypoints[i+1])
-        plot_path(environment, path)
-
-#simulate_agent()
