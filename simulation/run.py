8d602e48e15ba52d757f84267ac54dd2db09898e
Jonas Spinner <jonas.spinner@student.kit.edu>
2020-11-24 16:19:00 +0100

    Refactored run file.

--- simulation/run.py
+++ simulation/run.py
@@ -1,72 +1,67 @@
-from path import find_path, plot_path, build_graph
-from loader import load_environment
-from agent import Agent
-import matplotlib.pyplot as plot 
-import numpy as np 
-import networkx as nx
+from pathlib import Path
 
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib.colors import ListedColormap
+import numpy as np
 from matplotlib.animation import FuncAnimation
 
-building_cmap = ListedColormap([
-    [0.5, 0.5, 0.5, 1],  # wall
-    [1, 1, 1, 1],  # floor
-    [1, 0, 0, 1],  # seat
-    [0, 1, 0, 1],  # entry
-    [0, 0, 1, 1]  # food
-])
-
-runSaveFile = False
-res = 1
-environment, entryPosition, foodPosition, seatPositions = load_environment("map1", res)
-
-environmentGraph = build_graph(environment)
-
-agentPop = []
-#Create path to food place from entrence
-pathToFood = find_path(environmentGraph, entryPosition[0], foodPosition[0])
-
-#Create agents walk path. This is to avoide calculating the path each iteration.
-for x in seatPositions:
-    pathToSeat = find_path(environmentGraph, foodPosition[0], x)
-    pathExit = find_path(environmentGraph, x, entryPosition[0])
-    agentPop.append( Agent(pathIn = pathToFood+pathToSeat, pathExit = pathExit))
+from agent import Agent
+from loader import load_environment
+from path import find_path, build_graph, building_cmap
 
 
-#Plot code
-fig, ax1 = plt.subplots(figsize=(5, 3))
-#Plot the map
-height, width = environment.shape
-ax1.set(xlim=(0, width), ylim=(0, height))
-im = plt.imshow(environment, cmap=building_cmap)
+def main(save_file_to_disk=False, resolution=1):
+    input_map_path = Path(__file__).parent.parent / "data" / "test-map-1.txt"
+    output_video_path = Path(__file__).parent.parent / "run-animation.mp4"
 
-circleList = []
+    environment, entries, foods, seats = load_environment(str(input_map_path), resolution)
+    entry_position = entries[0]
+    food_position = foods[0]
 
-for agent in range(len(agentPop)):
-    tmpPosition = agentPop[agent].GetPosition()
-    circle = plt.Circle((tmpPosition[0], tmpPosition[1]), res/4, fc='k', fill=True)
+    graph = build_graph(environment)
 
-    circleList.append(circle)
-    ax1.add_patch(circleList[agent])
+    agents = []
+    # Create path to food place from entrance
+    path_to_food = find_path(graph, entry_position, food_position)
+
+    # Create agents walk path. This is to avoid calculating the path each iteration.
+    for seat_position in seats:
+        path_to_seat = find_path(graph, food_position, seat_position)
+        path_to_exit = find_path(graph, seat_position, entry_position)
+        agents.append(Agent(pathIn=path_to_food + path_to_seat, pathExit=path_to_exit))
+
+    # Plot code
+    fig, ax = plt.subplots(figsize=(5, 3))
+    # Plot the map
+    height, width = environment.shape
+    ax.set(xlim=(0, width), ylim=(0, height))
+    ax.imshow(environment, cmap=building_cmap)
+
+    circles = []
+
+    for i, agent in enumerate(agents):
+        y, x = agent.position
+        circle = plt.Circle((x, y), resolution / 4, fc='k', fill=True)
+
+        circles.append(circle)
+        ax.add_patch(circle)
+
+    def update_agents(frame):
+        for agent, circle in zip(agents, circles):
+            if agent.state == "OUT" and np.random.random() < 1 / len(agents):
+                agent.state = "GO_IN"
+
+            agent.Step()
+            y, x = agent.position
+            circle.center = (x, y)
+        return circles
+
+    if save_file_to_disk:
+        animation = FuncAnimation(fig, update_agents, interval=10, frames=100, repeat=False)
+        animation.save(str(output_video_path), fps=30, extra_args=['-vcodec', 'libx264'], dpi=300)
+    else:
+        animation = FuncAnimation(fig, update_agents, interval=10)
+    plt.show()
 
 
-def updateAgents(frame):
-
-    for agent in range(len(agentPop)):
-        if agentPop[agent].GetState() == "OUT" and np.random.random() < 1/len(agentPop):
-            agentPop[agent].SetState("GO_IN")
-
-        tmpPosition=agentPop[agent].Step()
-
-        circleList[agent].center = (tmpPosition[1],tmpPosition[0])
-
-    return circleList
-
-if runSaveFile:
-    animation = FuncAnimation(fig, updateAgents, interval=10, frames=1000, repeat=False)
-    animation.save('runAnimiation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-else:
-    animation = FuncAnimation(fig, updateAgents, interval=10)
-plt.show()
+if __name__ == '__main__':
+    main(save_file_to_disk=True)
