7c124768c3db1445e4710113d2287607ac0bc136
ericjohansson91 <61830151+ericjohansson91@users.noreply.github.com>
2020-11-25 17:30:06 +0100

    visibility and emit_particles should now work. Have not implemented them with run.py yet

--- simulation/emit_particles.py
+++ simulation/emit_particles.py
@@ -1,36 +1,47 @@
 import numpy as np
 
     
-def getDistances(visibilityMatrix : np.ndarray) -> np.ndarray:
+def getDistances(visibilityMatrix : np.ndarray, environment : np.ndarray) -> np.ndarray:
     """
     Parameters
     ----------
+    visibilityMatrix
         Holds all visibility maps corresponding to each positions in the environment
+    environment
+        Used for finding non zero element when initializing distanceMatrix
 
     Returns
     -------
     distanceMatrix : np.ndarray
         distanceMatrix describes the distance to all states in a radius of 2m 
         (this depends on visibilityMatrix) from a given state.  
-    """
+    """ 
     
-    #TODO: maybe collect all distanceMatrices in a large 3D matrix as in case with visibilies. 
+    # Look for a state in environment which is not a wall since those states in visibilityMatrix are empty
+    found = False
+    while found == False:
+        for i in range(2,20):
+            for j in range(5,20): 
+                if environment[i,j] != 0:
+                    distanceMatrix = np.full(np.shape(visibilityMatrix[int(i),int(j)]), np.inf, dtype=float)
+                    found = True
+      
+    # Calculate distances from the middle position of the matrix    
+    middlePos = [int(np.ceil(np.shape(distanceMatrix)[0]/2))-1, int(np.ceil(np.shape(distanceMatrix)[0]/2))-1]
+    iRows = np.linspace(0,np.shape(distanceMatrix)[0]-1,np.shape(distanceMatrix)[0])
+    iCols = np.linspace(0,np.shape(distanceMatrix)[1]-1,np.shape(distanceMatrix)[1])
     
-    distanceMatrix = np.inf + np.zeros(np.shape(visibilityMatrix[0,0]),dtype=float)
-    middlePos = [np.ceil(np.shape(distanceMatrix)[0]/2),np.ceil(np.shape(distanceMatrix)[0]/2)]
-    
-    for i in np.shape(distanceMatrix)[0]:
-        if i == middlePos[0]:
-            continue    
-        for j in np.shape(distanceMatrix)[1]:    
-            if j == middlePos[1] :
-                continue
-            distanceMatrix[i,j] = np.sqrt( (i-middlePos[0])**2 + (j-middlePos[1])**2 )
+    for i in iRows:
+        for j in iCols:    
+            if int(i) == middlePos[0] and int(j) == middlePos[1]:
+                distanceMatrix[int(i),int(j)] = np.inf
+            else:
+                distanceMatrix[int(i),int(j)] = np.sqrt( (i-middlePos[0])**2 + (j-middlePos[1])**2 )
             
     return distanceMatrix
             
             
-def emit(environment : np.ndarray, particleMatrix : np.ndarray, visibilityMatrix : np.ndarray, xPos : int, yPos : int, emissionRate : float)  -> np.ndarray:
+def emit(environment : np.ndarray, particleMatrix : np.ndarray, visibilityMatrix : np.ndarray, distanceMatrix : np.ndarray, xPos : int, yPos : int, emissionRate : float)  -> np.ndarray:
     """
     Parameters
     ----------
@@ -40,6 +51,8 @@ def emit(environment : np.ndarray, particleMatrix : np.ndarray, visibilityMatrix
         particleMatrix describes the amount of particles in every position of the map.
     visibilityMatrix : matrix 3D
         Holds all visibility maps corresponding to each positions in the environment
+    distanceMatrix : matrix
+        distance to all states in a radius of 2m from current state (same distance matrix for all states)
     posX, posY : int
         The current agents position coordinates
     emissionRate : float
@@ -51,12 +64,41 @@ def emit(environment : np.ndarray, particleMatrix : np.ndarray, visibilityMatrix
         particleMatrix describes the amount of particles in every position of the map.
     """           
     
-    emission = emissionRate * 1/getDistances(visibilityMatrix)
+    emission = emissionRate * 1/distanceMatrix
+    visibilityMap = visibilityMatrix[xPos,yPos]
+    # visibilityMap har 0 for non visible states, i.e no emission. 
+    emission = np.where(visibilityMap != 0, emission, 0)
     
-    rangeMin = int(np.floor(np.shape(visibilityMatrix[xPos,yPos])[0]/2))
-    rangeMax = int(np.ceil(np.shape(visibilityMatrix[xPos,yPos])[0]/2))
-    
-    particleMatrix[(xPos - rangeMin):(xPos + rangeMax),(yPos - rangeMin):(yPos + rangeMax)] = particleMatrix[(xPos - rangeMin):(xPos + rangeMax),(yPos - rangeMin):(yPos + rangeMax)] + emission
+    # Condition such that range is not outside of enviromental map (same dimensions as particle matrix) 
+    # Look left
+    if xPos >= np.ceil(np.shape(emission)[1]/2):
+        rangeColMin = int(np.floor(np.shape(visibilityMatrix[xPos,yPos])[1]/2))
+    else:
+        rangeColMin = int(xPos)
+        
+    # Look right
+    if np.shape(environment)[0]-xPos > np.ceil(np.shape(emission)[0]/2):
+        rangeColMax = int(np.floor(np.shape(visibilityMatrix[xPos,yPos])[1]/2))
+    else:
+        rangeColMax = int(np.shape(environment)[1]-xPos)
+        
+    # Look up
+    if yPos >= np.ceil(np.shape(emission)[1]/2):
+        rangeRowMin = int(np.floor(np.shape(visibilityMatrix[xPos,yPos])[0]/2))
+    else:
+        rangeRowMin = int(yPos)
+        
+    # Look down
+    if np.shape(environment)[1]-yPos > np.ceil(np.shape(emission)[1]/2):
+        rangeRowMax = int(np.floor(np.shape(visibilityMatrix[xPos,yPos])[0]/2))
+    else:
+        rangeRowMax = int(np.floor(np.shape(visibilityMatrix[xPos,yPos])[0]/2))
+        
+    # Takes care of cases where emission matrix gets outside of particleMatrix
+    particleMatrix[(xPos - rangeColMin):(xPos + rangeColMax),(yPos - rangeRowMin):(yPos + rangeRowMax)] = \
+        particleMatrix[(xPos - rangeColMin):(xPos + rangeColMax),(yPos - rangeRowMin):(yPos + rangeRowMax)] + \
+        emission[(int(np.ceil(np.shape(emission)[1]/2)) - rangeColMin):(int(np.ceil(np.shape(emission)[1]/2)) + rangeColMax), \
+                 (int(np.ceil(np.shape(emission)[0]/2)) - rangeRowMin):(int(np.ceil(np.shape(emission)[0]/2)) + rangeRowMax)]
     return particleMatrix
     
 def evaporation(particleMatrix : np.ndarray, evaporationMatrix : np.ndarray)  -> np.ndarray:
--- simulation/visibility.py
+++ simulation/visibility.py
@@ -27,39 +27,50 @@ def getVisibilityMaps(environment : np.ndarray, resolution : int)  -> np.ndarray
     """
     
     def getVisibility(environment : np.ndarray, resolution : int, row : int, col : int) -> np.ndarray:        
-        # how much is two meters? Assuming 1 square in original map is 50 cm. 
+        # how much is two meters? Assuming 1 (original) square in original map is 50 cm. 
         radius = 4*resolution
-        dRadius = np.linspace(0,radius,40)
-        dTheta = np.linspace(0,2*np.pi,40)
+        dRadius = np.linspace(0,radius/1.5,10)
+        dTheta = np.linspace(0,2*np.pi,10)
         visibilityMap = 2 + np.zeros((radius*2 + 1,radius*2 + 1),dtype=int)
         
         for theta in dTheta:            
-            for radius in dRadius:
+            for rad in dRadius:
                 
-                vRowPos = radius + np.sin(theta)*radius
-                vColPos = radius + np.cos(theta)*radius
-                environmentalRowPos = row + 0.5 + np.sin(theta)*radius
-                environmentalColPos = col + 0.5 + np.cos(theta)*radius
-                envState = environment[np.floor(environmentalRowPos),np.floor(environmentalColPos)]
+                vRowPos = radius + np.sin(theta)*rad
+                vColPos = radius + np.cos(theta)*rad
                 
-                if envState in [1,2,3,4]:
-                    visibilityMap[np.floor(vRowPos),np.floor(vColPos)] = 1
-                if envState == 0:
-                    visibilityMap[np.floor(vRowPos),np.floor(vColPos)] = 0
+                # Break if outside of map (case if entry/food is on boundry, this way food is enabled to be placed anywhere in the map)
+                environmentalRowPos = row + 0.5 + np.sin(theta)*rad              
+                if environmentalRowPos > np.shape(environment)[1] or environmentalRowPos < 0:
                     break
                 
+                environmentalColPos = col + 0.5 + np.cos(theta)*rad         
+                if environmentalColPos > np.shape(environment)[0]-1 or environmentalColPos < 0:
+                    break
+                
+                envState = environment[int(np.round(environmentalRowPos)),int(np.round(environmentalColPos))]                  
+                if envState in [1,2,3,4]:
+                    visibilityMap[int(np.round(vRowPos)),int(np.round(vColPos))] = 1
+                if envState == 0:
+                    visibilityMap[int(np.round(vRowPos)),int(np.round(vColPos))] = 0
+                    break
+                
+        # Checking for reasonable placement of starting point.
+        # visibilityMap[int(np.round(radius)), int(np.round(radius))] = 3   
+        
         visibilityMap[visibilityMap==2] = 0
         return visibilityMap
                     
     
-    nRow, nCol = environment.shape
+    nRow, nCol = np.shape(environment)
+    visibilityMatrix = np.zeros((nRow,nCol),dtype=object)
     nRow = np.linspace(0,nRow-1,nRow)
     nCol = np.linspace(0,nCol-1,nCol)
-    visibilityMatrix = np.zeros((nRow,nCol),dtype=object)
     # Loop though environment, add each positions visibility map
     for i in nRow:
         for j in nCol:
-            if environment[i,j] != 0:
-                visibilityMatrix[int(i),int(j)] = getVisibility(environment, resolution, int(i), int(j))
+            if environment[int(i),int(j)] != 0:
+                visibilityMap = getVisibility(environment, resolution, int(i), int(j))
+                visibilityMatrix[int(i),int(j)] = visibilityMap
     return visibilityMatrix
         
