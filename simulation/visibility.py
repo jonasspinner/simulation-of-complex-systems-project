f116fe493878bb00779be2e3878717bff2b96888
Jonas Spinner <jonas.spinner@student.kit.edu>
2020-12-08 17:58:42 +0100

    Refactored visibility calculation.

--- simulation/visibility.py
+++ simulation/visibility.py
@@ -1,11 +1,13 @@
 import numpy as np
+from loader import TileType
 
-def getVisibilityMaps(environment : np.ndarray, resolution : int)  -> np.ndarray:
+
+def getVisibilityMaps(environment: np.ndarray, resolution: int) -> np.ndarray:
     """
     Parameters
     ----------
     environment : matrix
-        In correct dimensions, i.e resolution has allready been applied
+        In correct dimensions, i.e resolution has already been applied
     resolution : int
         Use this for scale, i.e if we decide that one original square is 50x50 cm,
         then one square in the environment is 50/res x 50/res cm. Need this for 
@@ -16,69 +18,49 @@ def getVisibilityMaps(environment : np.ndarray, resolution : int)  -> np.ndarray
     matrix : np.ndarray
         Matrix with visibility matrices for all possible positions in environment. 
         1 for visible state (i.e a state to which particle emits from a given position)
-        0 for non visible states. 
-        
-        OBS: values of environmental matrix:    
-         WALL = 0
-         FLOOR = 1
-         SEAT = 2
-         ENTRY = 3
-         FOOD = 4
-         ONEWAY_UP = 5
-         ONEWAY_RIGHT = 6
-         ONEWAY_DOWN = 7
-         ONEWAY_LEFT = 8
-         TABLE = 9
-         WALKING_PATH = 10
-     """
-    
-    def getVisibility(environment : np.ndarray, resolution : int, col : int, row : int) -> np.ndarray:        
+        0 for non visible states.
+    """
+    width, height = environment.shape
+
+    def calculate_local_visibility(col: int, row: int) -> np.ndarray:
         # how much is two meters? Assuming 1 (original) square in original map is 50 cm. 
         radius = 4*resolution 
-        dRadius = np.linspace(0,radius/1.25,30)
-        dTheta = np.linspace(0,2*np.pi,50)
-        visibilityMap = 2 + np.zeros((radius*2 + 1,radius*2 + 1),dtype=int)
+        radii = np.linspace(0, radius/1.25, 30)
+        thetas = np.linspace(0, 2*np.pi, 50)
+        visibility_from_pos = 2 + np.zeros((radius*2 + 1, radius*2 + 1), dtype=int)
         
-        for theta in dTheta:            
-            for rad in dRadius:
-                
-                # -0.5 since radius does not take in acount that indexing starts at 0
-                vColPos = radius + 0.5 + np.cos(theta)*rad
-                vRowPos = radius + 0.5 + np.sin(theta)*rad
-                
-                # +0.5 because col and row takes into account that indexing starts at 0
-                # Break if outside of map (case if entry/food is on boundry, this way food is enabled to be placed anywhere in the map)
-                environmentalColPos = col + 0.5 + np.cos(theta)*rad              
-                if environmentalColPos >= np.shape(environment)[0] or environmentalColPos < 0:
+        for theta in thetas:
+            for rad in radii:
+                # Center of visibility calculation in local coordinates.
+                local_x = radius
+                local_y = radius
+
+                # -0.5 since radius does not take in account that indexing starts at 0
+                dx = int(0.5 + np.cos(theta) * rad)
+                dy = int(0.5 + np.sin(theta) * rad)
+                assert(-radius <= dx <= radius)
+                assert(-radius <= dy <= radius)
+
+                # Break if outside of map (case if entry/food is on boundary, this way food is enabled to be placed
+                # anywhere in the map)
+                if not (0 <= col + dx < width and 0 <= row + dy < height):
                     break
-                
-                environmentalRowPos = row + 0.5 + np.sin(theta)*rad         
-                if environmentalRowPos >= np.shape(environment)[1] or environmentalRowPos < 0:
+
+                tile_type = environment[col + dx, row + dy]
+
+                if tile_type == TileType.WALL or tile_type == TileType.TABLE:
+                    visibility_from_pos[local_x + dx, local_y + dy] = 0
                     break
-                
-                envState = environment[int(np.floor(environmentalColPos)),int(np.floor(environmentalRowPos))]                  
-                if envState in [1,2,3,4,5,6,7,8,10]:
-                    visibilityMap[int(np.floor(vColPos)),int(np.floor(vRowPos))] = 1
-                if envState == 0 or envState == 9:
-                    visibilityMap[int(np.floor(vColPos)),int(np.floor(vRowPos))] = 0
-                    break
-                
-        # Checking for reasonable placement of starting point.
-        # visibilityMap[int(np.round(radius)), int(np.round(radius))] = 3   
+                else:
+                    visibility_from_pos[local_x + dx, local_y + dy] = 1
         
-        visibilityMap[visibilityMap==2] = 0
-        return visibilityMap
-                    
-    
-    nCol, nRow = np.shape(environment)
-    visibilityMatrix = np.zeros((nCol,nRow),dtype=object)
-    nCol = np.linspace(0,nCol-1,nCol)
-    nRow = np.linspace(0,nRow-1,nRow)
+        visibility_from_pos[visibility_from_pos == 2] = 0
+        return visibility_from_pos
+
+    visibility = np.zeros((width, height), dtype=object)
     # Loop though environment, add each positions visibility map
-    for j in nCol:
-        for i in nRow:
-            if environment[int(j),int(i)] != 0 or environment[int(j),int(i)] != 9:
-                visibilityMap = getVisibility(environment, resolution, int(j), int(i))
-                visibilityMatrix[int(j),int(i)] = visibilityMap
-    return visibilityMatrix
-        
+    for x in range(width):
+        for y in range(height):
+            if environment[x, y] != TileType.WALL and environment[x, y] != TileType.TABLE:
+                visibility[x, y] = calculate_local_visibility(x, y)
+    return visibility
